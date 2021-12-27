e0225bf9a6314e1522d86974fcbc592d5bbad138
Judzhin Miles <info@msbios.com>
2021-12-27 17:15:31 +0200

    Fix

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(
+        private UserRepository $userRepository
+    )
+    {
+    }
+
     #[Route('/', name: 'dashboard')]
     public function index(): Response
     {
+        $user = $this->userRepository->findOneBy([], ['id' => 'ASC']);
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'total' => $this->userRepository->count([]),
+            'user' => $user
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,37 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = (new User);
+        $user->setPhone('+380000000000');
+        $user->setPassword('pass');
+        $user->setSurname('surName');
+        $user->setFirstName('firstNAme');
+        $manager->persist($user);
+        $manager->flush();
+
+        $user = (new User);
+        $user->setPhone('+380000000001');
+        $user->setPassword('pass');
+        $user->setSurname('surName1');
+        $user->setFirstName('firstNAme1');
+        $manager->persist($user);
+        $manager->flush();
+
+        $user = (new User);
+        $user->setPhone('+380000000002');
+        $user->setPassword('pass2');
+        $user->setSurname('surName2');
+        $user->setFirstName('firstNAme2');
+        $manager->persist($user);
+        $manager->flush();
+
+        $user = (new User);
+        $user->setPhone('+380000000003');
+        $user->setPassword('pass3');
+        $user->setSurname('surName3');
+        $user->setFirstName('firstNAme3');
+        $manager->persist($user);
+        $manager->flush();
 
         $manager->flush();
     }
